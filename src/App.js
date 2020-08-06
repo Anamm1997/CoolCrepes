17a725b87d4039f481ea678ff4e5dae4c247064b
Mahfuz Anam <anamm9510@gmail.com>
2020-08-06 11:47:32 -0400

    Added routing functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
+import Home from './components/Home';
+import LogoPage from './components/Logo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <div>
+          <NavLink to="/">Logo </NavLink>
+          <NavLink to="/home">Home </NavLink>
+            <Switch>
+             <Route path="/" component={LogoPage} exact/>
+             <Route path="/home" component={Home} exact/>
+           </Switch>
+        </div> 
+      </BrowserRouter>
     </div>
   );
 }
