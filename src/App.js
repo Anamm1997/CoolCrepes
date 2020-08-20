3272c2a26638ae346698889589840826c3bda3ff
LindaRZhang <Linda1295841705@gmail.com>
2020-08-19 23:41:35 -0400

    complete Homepage

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import LoginPage from './components/LoginPage'
 import RegisterPage from './components/RegisterPage'
 import UserPage from './components/UserPage'
 import CartPage from './components/CartPage'
+import SalesPage from "./components/SalesPage"
+import TrendingPage from "./components/TrendingPage"
 import './App.css';
 
 class App extends React.Component {
@@ -34,6 +36,9 @@ class App extends React.Component {
               <Route path="/cart" component={CartPage} exact/>
               <Route path="/login" component={LoginPage} exact/>
               <Route path="/register" component={RegisterPage} exact/>
+               <Route path="/sale" component={SalesPage} exact/>
+              <Route path="/trending" component={TrendingPage} exact/>
+
             </Switch>
           </div>
         </BrowserRouter>
---
+++ src/components/SalesPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+ 
+class SalesPage extends React.Component {
+    render() {
+        return (
+            <div>
+               <h1>SalesPage</h1>
+                <p>Deals</p>
+            </div>
+         );
+    }
+}
+ 
+export default SalesPage;
