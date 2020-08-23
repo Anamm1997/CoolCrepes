75ea2f24ee46b41bd8af0f67662186903b371ec2
Ricky102 <48187498+Ricky102@users.noreply.github.com>
2020-08-23 18:19:31 -0400

    Renamed branch to ProductPage. Deleted UserPage.js in src/components/UserPage.js as already exists in src/components/User. Create db reference in ProductPage.js

--- src/components/Products/ProductsPage.js
+++ src/components/Products/ProductsPage.js
@@ -1,11 +1,41 @@
 import React from 'react';
- 
+//import CSS page
+import Fire from '../Fire';
+
+//Import Admin SDK
+var admin = require('firebase-admin');
+
+//Get a database reference to our posts
+var db = admin.database()
+var ref = db.ref(''); //Url for our database goes in here?
+
+//Attach an asynchronous callback to read the data at our posts reference - https://firebase.google.com/docs/database/admin/retrieve-data#node.js
+ref.on('value', function(snapshot) {
+	console.log(snapshot.val());
+}, function(errorObject) {
+	console.log('The read failed: ' + errorObject.code);
+});
+
 class ProductsPage extends React.Component {
+	constructor(props) {
+		super(props);
+		//methods
+
+		this.state={
+			Product:"",
+			Price:""
+		}
+	}
+
+	//What method to grab data from database?
+
+	//Learn react here to determine how to interact with page, display table, instead of standard HTML and JS.
     render() {
         return (
             <div>
                <h1>ProductsPage</h1>
                 <p>ProductsPage body content. Will have a list of products, have sorting and filtering options, initilized filtering through the query params.</p>
+                <table id='products'></table>
             </div>
          );
     }
