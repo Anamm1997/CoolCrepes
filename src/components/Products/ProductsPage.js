a108b2e1b49d57d88b52c50cf5ccae2344118b0f
Ricky102 <48187498+Ricky102@users.noreply.github.com>
2020-08-29 18:33:20 -0400

    Working off of Linda's code. Now displaying data in a table.

--- src/components/Products/ProductsPage.js
+++ src/components/Products/ProductsPage.js
@@ -1,27 +1,8 @@
 import React from 'react';
 //import CSS page
 import Fire from '../Fire';
-//import * as admin from firebase-admin;
+import { Table } from 'reactstrap';
 
-/*/Import Admin SDK
-var admin = require('firebase-admin');
-try {
-    admin.initializeApp();
-} catch(error) {
-    //TODO: ignoring until firebase-functions fix released
-    console.log('Initialize not working.');
-}
-
-//Get a database reference to our posts
-var db = admin.database()
-var ref = db.ref(''); //Url for our database goes in here?
-
-//Attach an asynchronous callback to read the data at our posts reference - https://firebase.google.com/docs/database/admin/retrieve-data#node.js
-ref.on('value', function(snapshot) {
-	console.log(snapshot.val());
-}, function(errorObject) {
-	console.log('The read failed: ' + errorObject.code);
-});*/
 class ProductsPage extends React.Component {
 	constructor(props) {
 		super(props);
@@ -63,28 +44,45 @@ class ProductsPage extends React.Component {
         });   
 	}
 
-
-
-	//Learn react here to determine how to interact with page, display table, instead of standard HTML and JS.
     render() {
         return (
             <div>
                <h1>ProductsPage</h1>
                 <p>ProductsPage body content. Will have a list of products, have sorting and filtering options, initialized filtering through the query params.</p>
 
-                <button onClick={()=>{ this.retrieve() }}> Button does nothing </button>
+                <button onClick={()=>{ this.retrieve() }}> Display </button>
+                <Table bordered>
+             		<thead>
+             			<tr>
+             				<th>#</th>
+             				<th>item</th>
+             				<th>product</th>
+             				<th>price</th>
+             			</tr>
+             		</thead>
+             		<tbody>
 
          {this.state.productList.map((item,index)=>{ 
              console.log(item)
              return(
-                 <div>
-            <p>{item} {item.product} {item.price}</p>
-                 </div>
+             	<tr>
+             		<th scope='row'>1</th>
+             		<td>{item}</td>
+             		<td>{item.product}</td>
+             		<td>{item.price}</td>
+             	</tr>
+                 
          )})}
+         			</tbody>
+         			</Table>
             </div>
 
          );
     }
 }
-
+/*
+<div>
+<p>{item} {item.product} {item.price}</p>
+ </div>
+*/
 export default ProductsPage;
