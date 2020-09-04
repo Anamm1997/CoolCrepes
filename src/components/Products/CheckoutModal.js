a4fb0ea9da1b8973faa99a4661d789a0d0da89a6
LindaRZhang <Linda1295841705@gmail.com>
2020-09-04 17:40:16 -0400

    update total price and quantity, working on purchase db

--- src/components/Products/CheckoutModal.js
+++ src/components/Products/CheckoutModal.js
@@ -8,64 +8,53 @@ class CheckoutModal extends React.Component {
         super(props);  
         this.itemsPurchased = this.itemsPurchased.bind(this);
         this.purchasesRef = Fire.database().ref(`user/${this.props.propId}/purchase`);
+        this.GetCart = this.GetCart.bind(this);
         this.state = {
             currentCart:[],
             totalPrice:0,
             redirect: false,
         }
     }
-
+    
     componentWillMount() {
         this.props.onRef(this)
-        var prices = 0;
-        const cartItems = [];
-        Fire.database().ref(`user/${this.props.propId}/cart`).on('value',function (snapshot){
-            let items = snapshot.val();
-            for(let id in items){
-                cartItems.push({id,...items[id]});
-                prices = parseFloat(prices) + (parseFloat(items[id].price) * parseFloat(items[id].quantity));
-            }
-        });
-        prices = prices.toFixed(2);
+        Fire.database().ref(`user/${this.props.propId}/cart`).on('value', this.GetCart.bind(this));
+    }
+    
+    componentWillUnmount() {
+        Fire.database().ref(`user/${this.props.propId}/cart`).off('value', this.GetCart.bind(this));
+    }
+    
+    GetCart(snapshot) {
+        let items = snapshot.val();
+        let prices = 0;
+        for(let item in items){
+            prices += (parseFloat(items[item].price) * parseInt(items[item].quantity));
+        }
         this.setState({
-            currentCart: {cartItems}
-        });
-        this.setState({
+            currentCart: items,
             totalPrice: prices
         });
     }
-
+    
     async itemsPurchased(){
         if(this.state.totalPrice == 0){
             alert("Your cart is empty")
-        }
+           }
         else{
-            let cartObj = {};
-            console.log(this.state.currentCart)
-            for(let i = 0; i <= this.state.currentCart; i++) {
-                let cart = this.state.currentCart[i];
-                cartObj[cart.id] = {
-                    quantity: cart.quantity,
-                    price: cart.price,
-                    productName: cart.productName,
-                    imageURL: cart.imageURL,
-                    discount: cart.discount
-                };
-            }
-            console.log("cartobg"+cartObj)
             let purchasedCart = {
-                cart: cartObj,
+                cart: this.state.currentCart,
                 timeStamp: Date.now(),
                 totalPrice: this.state.totalPrice,
                 userID:this.props.propId
             }
-            console.log("purchased cart"+purchasedCart)
+            console.log(purchasedCart)
             //add to purchase
             let userPurchases= (await Fire.database().ref(`user/${this.props.propId}`).once('value')).val().purchase;
-            let purchaseRef = Fire.database().ref(`purchase`).push(purchasedCart)
+            let purchaseRef = Fire.database().ref(`user/${this.props.propId}/purchase`).push(purchasedCart)
             if(!userPurchases) {userPurchases = []}
             userPurchases.push(purchaseRef.key);
-
+            
             //remove cart
             Fire.database().ref(`user/${this.props.propId}/cart`).remove();
             this.setState({redirect:!this.state.redirect})
@@ -75,7 +64,7 @@ class CheckoutModal extends React.Component {
     render(){
         if(this.state.redirect){
             return <Redirect to='/thanks'/>;
-        }
+           }
         return(
             <Modal isOpen={this.props.purchase} toggle={this.props.purchased}>
             <ModalHeader className="modalheaderAddtoCart" toggle={this.props.purchased}>Total: ${this.state.totalPrice}</ModalHeader>
