8b6b973e8cc864ef7a31f243bf71d3de9384492a
Mahfuz Anam <anamm9510@gmail.com>
2020-09-04 17:06:50 -0400

    resaving changes

--- src/components/Products/ProductsPage.js
+++ src/components/Products/ProductsPage.js
@@ -33,23 +33,9 @@ class ProductsPage extends React.Component {
     }
     
     add(items){
-<<<<<<< HEAD
         let quantity = 1;
         if(Object.keys(this.state.userCart).includes(items.id)) {
             quantity = 1 + this.state.userCart[items.id].quantity;
-=======
-        console.log("ITEMSSS"+items)
-        let id = items[5];
-        console.log("id"+id)
-
-        let newItem = {
-            id:{
-                'productName':items[0],
-                'price':items[1],
-                'quantity':1,
-                'imageURL':items[4],
-            }
->>>>>>> e56f78310c7bec95b95688c9c36e40d672016dce
         }
 
         let cartItem = {...this.state.userCart};
@@ -101,7 +87,6 @@ class ProductsPage extends React.Component {
     }
     
     componentDidMount() {
-<<<<<<< HEAD
         Fire.database().ref('product').on('value', this.retrievePastries);   
         if(this.props.userToken) {
             Fire.database().ref(`user/${this.props.userToken.id}`).on('value', this.readUserCart);
@@ -113,30 +98,6 @@ class ProductsPage extends React.Component {
         if(this.props.userToken) {
             Fire.database().ref(`user/${this.props.userToken.id}`).off('value', this.readUserCart);
         }
-=======
-        let list = []
-        Fire.database().ref('product').on('value', function(snapshot) {
-            var pastries = snapshot.val();
-            var keys = Object.keys(pastries);
-
-            for (var i = 0; i < keys.length; i++) {
-                var k = keys[i];
-                var description = pastries[k].description;
-                var price = pastries[k].price;
-                var product = pastries[k].productName;
-                var seller = pastries[k].seller;
-                var image = pastries[k].imageURL;
-        let data = [
-          product, price, seller, description, image, k
-        ]
-        console.log(data)
-                list.push(data)
-            }
-        })
-         this.setState({
-            productList : list,
-        });   
->>>>>>> e56f78310c7bec95b95688c9c36e40d672016dce
     }
 
     render() {
@@ -164,7 +125,6 @@ class ProductsPage extends React.Component {
              			</tr>
              		</thead>
              		<tbody>
-<<<<<<< HEAD
                         {this.state.productList.map((item,index)=>{ 
                             if (!item.isSelling) {
                                 return null;
@@ -196,34 +156,6 @@ class ProductsPage extends React.Component {
                             }
                         }
                         )}
-=======
-
-         {this.state.productList.map((item,index)=>{ 
-             return(
-
-             	<tr>
-             		<th scope='row'>1</th>
-                    <td><img src={item[4]} className="img-thumbnail shadow-sm" alt="sample image for now"/></td>
-             		<td>{item[0]}</td>
-             		<td>{item[1]}</td>
-             		<td></td>
-             		<td>{item[2]}</td>
-             		<td>{item[3]}</td>
-                    <td>
-             <Button color="btn btn-light" onClick={()=> {this.add(item)}}>Add</Button>
-<Modal isOpen={this.state.modal} toggle={this.toggle.bind(this)} >
-    <ModalHeader className="modalheaderAddtoCart" toggle={this.toggle.bind(this)}>Product Added to Cart</ModalHeader>
-<ModalFooter className="modalfooterAddtoCart">
-    <Button color="light" onClick={this.cartPage.bind(this)}>Cart</Button>{' '}
-<Button color="warning" onClick={this.purchased.bind(this)}>Proceed to Checkout</Button>
-</ModalFooter>
-</Modal>
-             <CheckoutModal onRef={ref => (this.child = ref)} propId={this.props.userToken.id} purchase={this.state.purchase} purchased={this.purchased.bind(this)}/>
-                 </td>
-             	</tr>
-
-         )})}
->>>>>>> e56f78310c7bec95b95688c9c36e40d672016dce
          			</tbody>
          			</Table>
             </div>
@@ -250,7 +182,6 @@ class ProductsPage extends React.Component {
          {this.state.productList.map((item,index)=>{ 
              return(
 
-<<<<<<< HEAD
                 <tr key={item.id}>
                     <th scope='row'>1</th>
                     <td><img src={item.imageURL} className="img-thumbnail shadow-sm" alt="sample image for now"/></td>
@@ -259,16 +190,6 @@ class ProductsPage extends React.Component {
                     <td className='text-success'>-{item.discount * 100}%</td>
                     <td>{item.stockQuantity}</td>
                     <td>{item.description}</td>
-=======
-             	<tr>
-             		<th scope='row'>1</th>
-                    <td><img src={item[4]} className="img-thumbnail shadow-sm" alt="sample image for now"/></td>
-             		<td>{item[0]}</td>
-             		<td>{item[1]}</td>
-             		<td></td>
-             		<td>{item[2]}</td>
-             		<td>{item[3]}</td>
->>>>>>> e56f78310c7bec95b95688c9c36e40d672016dce
              	</tr>
 
          )})}
